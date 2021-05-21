1b52844f2887992ade99f06f2adf569491dd762e
Dragos Mihai <mihaidragosradu@gmail.com>
2021-05-21 15:42:40 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
